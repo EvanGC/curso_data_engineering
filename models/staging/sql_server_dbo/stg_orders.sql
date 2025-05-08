b261029c1d77e9af7a1a4dad9afb5a78bebe4925
unknown <de05@civica-soft.com>
2025-05-08 19:58:43 +0200

    feat: add hashes to promo_id

--- models/staging/sql_server_dbo/stg_orders.sql
+++ models/staging/sql_server_dbo/stg_orders.sql
@@ -27,6 +27,26 @@ renamed as (
 
     from source
 
+), final as(
+
+    select
+        order_id,
+        shipping_service,
+        shipping_cost,
+        address_id,
+        created_at,
+        {{ dbt_utils.generate_surrogate_key(['promo_id']) }} as promo_id,
+        estimated_delivery_at,
+        order_cost,
+        user_id,
+        order_total,
+        delivered_at,
+        tracking_id,
+        status,
+        _fivetran_deleted,
+        _fivetran_synced
+
+    from renamed
 )
 
-select * from renamed
+select * from final
